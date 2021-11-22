1cdb87816608c9228a85010dcf46a368cb88cf90
Nicemine <alessandro.maioli@gmail.com>
2021-11-22 11:32:40 +0100

    added structures for new probe-worker event format

--- queue/messages.go
+++ queue/messages.go
@@ -76,3 +76,46 @@ type WsOrderTradeUpdate struct {
 	CallbackRate         string `json:"cr"`
 	RealizedPnL          string `json:"rp"` // required
 }
+
+// NEW EVENT DATA STRUCTURES
+
+type RmqProbeEvent struct {
+	UserId    string      `json:"userId"`
+	BotId     string      `json:"botId"`
+	AccessKey string      `json:"accessKey"`
+	SecretKey string      `json:"secretKey"`
+	EventType string      `json:"eventType"` //ACCOUNT_UPDATE or ORDER_UPDATE
+	EventTime string      `json:"eventTime"`
+	Data      interface{} `json:"data"`
+}
+
+type RmqAccountUpdateData struct {
+	Balances  *[]RmqAccountUpdateBalance
+	Positions *[]RmqAccountUpdatePosition
+}
+
+type RmqAccountUpdateBalance struct {
+	Asset        string  `json:"asset"`
+	AssetBalance float64 `json:"assetBalance"`
+}
+
+type RmqAccountUpdatePosition struct {
+	Symbol         string  `json:"symbol"`
+	InstrumentType string  `json:"instrumentType"`
+	PositionAmount float64 `json:"positionAmount"`
+	PositionSide   string  `json:"positionSide"`
+	EntryPrice     float64 `json:"entryPrice"`
+}
+
+type RmqOrderUpdateData struct {
+	Symbol         string  `json:"symbol"`
+	InstrumentType string  `json:"instrumentType"`
+	OrderId        string  `json:"orderId"`
+	TradeTime      string  `json:"tradeTime"`
+	PositionSide   string  `json:"positionSide"`
+	FilledQuantity float64 `json:"filledQuantity"`
+	OrderStatus    string  `json:"orderStatus"`
+	OriginalPrice  float64 `json:"originalPrice"`
+	ExecutionPrice float64 `json:"executionPrice"`
+	RealizedPnl    float64 `json:"realizedPnl"`
+}
