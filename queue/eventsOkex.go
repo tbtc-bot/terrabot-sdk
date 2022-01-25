86f5f74ad5a8dc5bf85ba3d8799ae3d3f340c9f5
Emanuele Bolognesi <35916369+adrift942@users.noreply.github.com>
2022-01-25 15:59:45 +0100

    removed Currency and PositionCurrency from RmqAccountUpdatePosition

--- queue/eventsOkex.go
+++ queue/eventsOkex.go
@@ -23,13 +23,11 @@ type RmqAccountUpdateBalance struct {
 }
 
 type RmqAccountUpdatePosition struct {
-	Symbol           string  `json:"symbol"`
-	InstrumentType   string  `json:"instrumentType"`
-	PositionAmount   float64 `json:"positionAmount"`
-	PositionSide     string  `json:"positionSide"`
-	EntryPrice       float64 `json:"entryPrice"`
-	Currency         string  `json:"ccy"`
-	PositionCurrency string  `json:"posCcy"` // this is needed by okex margin to determine the position side
+	Symbol         string  `json:"symbol"`
+	InstrumentType string  `json:"instrumentType"`
+	PositionAmount float64 `json:"positionAmount"`
+	PositionSide   string  `json:"positionSide"`
+	EntryPrice     float64 `json:"entryPrice"`
 }
 
 type RmqOrderUpdateData struct {
